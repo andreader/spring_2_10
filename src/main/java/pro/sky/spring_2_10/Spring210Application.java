26b21b15a8c54f3f1de8f156401cb3d09541fa40
Андрей <79538233369@ya.ru>
2024-05-13 17:47:55 +0500

    master

---
+++ src/main/java/pro/sky/spring_2_10/Spring210Application.java
@@ -0,0 +1,13 @@
+package pro.sky.spring_2_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring210Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring210Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/spring_2_10/Spring210ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.spring_2_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring210ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
